f228ad08cb3a56a4c38ae145f27ba62f5cd23d8e
BUYSELL-Japan <buyselljapan0311@gmail.com>
2025-08-21 06:02:05 +0900

    Update name.test.js

--- project/src/components/name.test.js
+++ project/src/components/name.test.js
@@ -2,9 +2,6 @@ import { describe, test, expect } from 'vitest';
 
 describe('Component-Name', () => {
   test('コンポーネントが正しくレンダリングされることを確認します', () => {
-    // ここにテストのロジックを記述します
-    // 例: expect(コンポーネントのプロパティ).toBe('期待する値');
-
     // これは、テストが動作することを確認するためのプレースホルダーです。
     expect(true).toBe(true);
   });
